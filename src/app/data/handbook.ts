21a0974a21e6835bd3cc1ad7bcf733b6d0db0519
Adeyossy <adeyossy1@gmail.com>
2023-03-28 06:21:22 +0100

    feat: working on the handbook file structure

--- src/app/data/handbook.ts
+++ src/app/data/handbook.ts
@@ -4,7 +4,7 @@ export const handbook = [
     content: [
       {
         metatype: "paragraph",
-        items: [
+        content: [
           "I have the honour of being the editor for the second version of the family medicine residency training handbook. It has been 11 years since the first edition (November 2011). There have been considerable improvements to the residency training while the challenges were surmounted as they came. Each challenge provided a learning opportunity.",
           "Going through the previous Editor’s Note, one realises the historical perspective that it holds, hence its inclusion in the history section of this handbook.",
           "The initiative to revise the handbook came from the Head of Department, Dr Adedotun A Adetunji. He set up the Residency Handbook Review Committee in October 2022, comprising of the Residency Training Coordinator, Dr Kehinde Adigun, Dr ‘Bimbo Olorunsola (CME coordinator), Dr Oluwatoyin Olalemi, Dr Oyindamola Aderinto (Chief Resident), Dr Onaopepomipo Kolade (Senior Registrar), Dr Eniade Boriwaye (Registrar) and I. Even though Dr ‘Folajimi Senjobi was not part of the committee, he played a very important role in this edition especially for the abstracts of previous dissertations, a new feature added in this 2nd edition. Dr Olushola Mosuro coordinated the pictures, not a small feat!"
@@ -12,24 +12,22 @@ export const handbook = [
       },
       {
         metatype: "subheading",
-        items: {
-          subheading: "What is new in this 2nd edition?",
-          content: [
-            {
-              metatype: 'list',
-              items: [
-                "We decided to make it into an application that would make access to the handbook possible on our phones and PCs with the printable forms feature and easier navigation.",
-                "The handbook also reflects the updated 2023 curriculum of both postgraduate medical colleges",
-                "You will be able to access abstracts of previous dissertations to have a better feel of what was done and expand on these research studies to further develop them.",
-                "There are more pictures so that the new resident doctor can easily spot areas of training and trainers to make the transition as a new trainee easier."
-              ]
-            }
-          ]
-        }
+        content: [
+          "What is new in this 2nd edition?"
+        ]
+      },
+      {
+        metatype: 'list',
+        content: [
+          "We decided to make it into an application that would make access to the handbook possible on our phones and PCs with the printable forms feature and easier navigation.",
+          "The handbook also reflects the updated 2023 curriculum of both postgraduate medical colleges",
+          "You will be able to access abstracts of previous dissertations to have a better feel of what was done and expand on these research studies to further develop them.",
+          "There are more pictures so that the new resident doctor can easily spot areas of training and trainers to make the transition as a new trainee easier."
+        ]
       },
       {
         metatype: "paragraph",
-        items: [
+        content: [
           "We still have many more ideas we would like to include and feel sure that an electronic version makes subsequent editions easier to upgrade, hence answering the needs of the doctor who chose to train in the speciality of family medicine in the University College Hospital Ibadan.",
           "Thank you.",
           "Dr. Emmanuelle Achiaka Irabor (FWACP-FM, MSc Develop Psych).",
@@ -45,7 +43,7 @@ export const handbook = [
     content: [
       {
         metatype: "paragraph",
-        items: [
+        content: [
           "This 2023 revision of University College Hospital, Ibadan (UCH) Family Medicine residency training handbook has a truly rich history spanning over 40 years of postgraduate training behind it.",
           "From the early days of handwritten guides for a lone pioneer resident, our training handbook has evolved through typewritten formats and paperback print to the current presentation as an electronic application.",
           "This digital aid to Family Medicine training is informed not just by fanciful pursuit of content upgrade, but by collective insight gleaned from the training program’s defiance of several odds over decades (including the COVID-19 pandemic recently).",
@@ -69,26 +67,26 @@ export const handbook = [
     content: [
       {
         metatype: "paragraph",
-        items: [
+        content: [
           "This department has been in existence since the establishment of the hospital in 1957. It was then known as the General Practice Clinic and was manned by Hospital Medical Officers. This was the major service area of the Hospital and it had a dual function."
         ]
       },
       {
         metatype: "list",
-        items: [
+        content: [
           "To regulate admissions within the hospital and attendance at any of the specialty clinics",
           "To provide the condition upon which the patients may be so admitted or attended to."
         ]
       },
       {
         metatype: "italic",
-        items: [
+        content: [
           "In the course of this arrangement, it was observed that the teaching potential in undergraduate medicine in the department was excellent, thus in 1968, a teaching unit with a side laboratory was added to cater for teaching medical students."
         ]
       },
       {
         metatype: "paragraph",
-        items: [
+        content: [
           "The clinic also attended to non-trauma emergency cases and was open for twenty-four hours. During the years of the Nigeria civil war (6th July 1967 - 15th January 1970) and the attendant disruption in health services, there was a large influx of patients and the after-hours emergency service had to be stopped due to work overload on the staff.",
           "This service was subsequently transferred to the Casualty Department. The name of the clinic was the therefore changed to the General Outpatients Department. It catered for all members of the public with or without referral letters and members of staff and their families. The services to the general public, staff and staff families, were eventually separated as two departments with their own staff complement as a result of improved structural organization. The General Out-patients Department apart from providing quality care to the general public eventually would commence the post graduate training in Family Medicine, whilst the Staff Medial Services provided for the staff and their families.",
           "The clinic thus gave medical care to a large number of people and afforded the junior medical staff the opportunity to gain experience in the diagnosis and treatment of many conditions that did not require specialist or in-patient care. They also learnt the early recognition of seriously ill patients.",
@@ -97,13 +95,13 @@ export const handbook = [
       },
       {
         metatype: "italic",
-        items: [
+        content: [
           "Subsequently, the junior medical staff cadre was replaced by registrars from the Department of Medicine because of this learning environment."
         ]
       },
       {
         metatype: "paragraph",
-        items: [
+        content: [
           "The UCH Ibadan, was accredited to train postgraduate resident doctors in General Medical Practice (GMP) in 1980. The General Outpatient Department (GOPD) was the obvious department of choice for training GMP resident doctors.",
           "With the change of specialty name from GMP to Family Medicine at the turn of the millennium, the name of the department was eventually changed to Department of Family Medicine in 2005.  A more patient-centered type of care was instituted to further improve patient satisfaction. The Department of Family Medicine attended to about 60,000 patients annually before the Covid-19 pandemic. The numbers had since dropped and hopefully will pick up again.",
           "Family Medicine is the medical specialty that provides comprehensive and continuing health care for the individual in a holistic manner within the context of his family and environment.  It encompasses all ages, both sexes, and every disease entity, integrating biological, clinical and behavioral sciences.  It provides primary care and in Nigeria, the term “Frontline Doctor” has been adopted.",
@@ -112,8 +110,110 @@ export const handbook = [
       },
       {
         metatype: "link",
-        items: [
+        content: [
           "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2239200/?page=1"
+        ]
+      },
+      {
+        metatype: "history",
+        content: [
+          {
+            metatype: "heading",
+            content: [
+              "A Department of General Practice in West Africa."
+            ]
+          },
+          {
+            metatype: "paragraph",
+            content: [
+              `It is the policy of the College of General Practitioners to advocate the formation of a Department of General Practice in each of our teaching hospitals, and it may therefore be of interest to describe how such a department is actually being developed in the new teaching hospital of University College, Ibadan, Nigeria.
+              This college, which has been built since the last war, is in special relationship with the University of London and its students take the London examinations and degrees; it is the only college in West Africa with a Faculty of Medicine.
+              General practice in Nigeria is very different from that in England, though the assortment of clinical problems, major and minor, presented by patients is very similar, and, I suppose, typical of general practice the world over. In Nigeria there are very few doctors working outside the government hospitals (in Ibadan with an estimated population 700,000 there are, I think, seven)and except among the Europeans and the wealthy Africans and Syrians there is no domiciliary visiting. The great majority of the sick, therefore, attend a general out-patient clinic at the nearest government or mission hospitals have 100 to 150 every morning, and at the old hospital being used temporarily by University College Hospital about 300 adults and 300 children attend daily. As a corollary to this, almost all the patients seen in the specialist clinics and in the wards have come through the general out-patient clinic, instead of from general practitioners outside the hospital.
+              It is this general out-patient clinic which the Board of Management on the recommendation of the Medical Advisory Committee has made into the Department of General Practice and it does, of course, provide an ideal opportunity for teaching the type of medicine which most of the graduates will be practicing when they leave hospital.
+              The new teaching hospital of over 500 beds is now almost completed, the various departments are being fitted out, and it’s expected to start functioning at the beginning of April. In its African setting it is almost fantastically modern both in building and equipment; and from kitchens, automatic telephone exchange, and lifts, towards, theatres and laboratories, has the most up-to-date fittings.
+              The Department of General Practice occupies the first of a series of blocks along the out-patient corridor. It is next to the pharmacy, where all prescriptions will be dispensed, and to the out-patient X-ray department and casualty department are adjacent.
+              The lay-out of the department itself is not ideal and experience may show the necessity for some changes, but it does in effect form a "health centre" with accommodation for six doctors. The medical equipment is all that could be desired and there is a staff or nurses, with a sister in charge and clerks, porters, etc. There is a clinical side-room fully equipped, and direct access to all laboratory and X. ray facilities. A system of record-keeping has been worked out with a method of obtaining statistics of diseases, attendances, and other factors. It will be (as indeed it should be in a teaching hospital) practice under almost ideal conditions, provided that the number of the medical staff is adequate to cope with the numbers of patients without undue pressure. It is to be hoped that training under these conditions will stimulate medical officers to try to attain similar conditions in their future work, in other less well equipped hospitals in the country.
+              The medical staff of the department will be of three grades
+              `
+            ]
+          },
+          {
+            metatype: "list",
+            content: [
+              "Two S.H.M.O.'s, experience in general practitioners work, who will be responsible for the organization (and incidentally also for the hospital staff medical services) and who will supervise, advise and teach the junior staff",
+              "Two LH.M.O's or senior house officers who will work in the department for possibly a year to gain further general experience, and",
+              "House physicians."
+            ]
+          },
+          {
+            metatype: "paragraph",
+            content: [
+              `The professor of Medicine has agreed that each of his house physicians will spend two out of his six months in the General practice unit. This is, I think, unparalleled and quite impossible in England, and should provide most valuable training.
+              In addition to this, it has been agreed that all students should attend the department at two stages in their clinical course. Hitherto the conditions for clinical teaching in Ibadan have not been such as to comply with the standards of the University of London and students have had to take their clinical course in the United Kingdom, where, of course, they do not see much of the type of medicine they will meet in their own country. It is to meet this lack that the new hospitals has been built, and the first clinical year is due to start there next October.
+              The first stage at which students will come is in the introductory clinical course in the autumn term. It has been planned for them to attend for one session in groups of six, to be shown the working of the clinic, why and how patients first come to the hospital, and how those they will be seeing later in the wards and specialist clinics are selected in other words to give them some perspective in medicine.
+              The second stage will be towards the end of their clinical course while working in the department of medicine. They will then be taken in groups of three, and will work in the clinic each morning for a week (a morning in Nigeria is from 8 a.m. to 2 p.m.) seeing patients themselves, writing up the notes, and carrying out arranging the necessary investigations the patients have to return to the clinic, it will be arranged for them to see the same students again, if they are referred to consultative clinics, or admitted, the students will follow them up.
+              During this week, I plan to give talks on the ethics of the doctor-patient relationship, and on minor ailments. One further possibility which is under consideration is to send students out with the district nurse tutor and her student nurses to see patients in their homes, a possibility which might lead eventually to the development of domiciliary visiting in this country.
+              Much of what I have described is still in the planning stage and I hope that it will not be considered premature to have written about it. The department has, however been formed and is doing its clinical work, though under very cramped and overcrowded conditions at present. The planning of its organization as part of the new hospital has been most interesting work, tinged rather with some apprehension as the opening day approaches. It is exciting to be taking part in the development of a great project like this new medical school, and especially to have found out here that the importance of general practice is recognized, and to have been given the opportunity to organize it and teach it.
+              `
+            ]
+          }
+        ]
+      },
+      {
+        metatype: "section",
+        content: [
+          {
+            metatype: "subheading",
+            content: [
+              "Posterity has recognised the importance of the Family Doctor."
+            ]
+          },
+          {
+            metatype: "section",
+            content: [
+              {
+                metatype: "paragraph",
+                content: [
+                  "So even though we might be a newer speciality, we were the first doctors and our worth appreciated. Please read the Address of Dr Kenneth Mclean Cobban (MB, ChB – 1927 (Uni Liverpool), MD (1930), MB, BS (1938 - Uni London) our 1st HoD on this issue."
+                ]
+              },
+              {
+                metatype: "link",
+                content: [
+                  "https://www.bmj.com/content/2/4740/1149.2"
+                ]
+              },
+              {
+                metatype: "image",
+                content: [
+                  "../assets/cobban.jpg"
+                ]
+              }
+            ]
+          },
+          {
+            metatype: "section",
+            content: [
+              {
+                metatype: "paragraph_link",
+                content: [
+                  `Our 2nd  Head of Department (HoD) was Dr Shun Reddy 1965 - 1971, A South African Indian who eventually who was an Associate Lecturer in the Department of Medicine of the University of Ibadan and later became a Professor of Community Medicine in the Ahmadu Bello University Zaria, Nigeria. 
+                  https://www.bmj.com/content/281/6253/1502
+                  `
+                ]
+              },
+              {
+                metatype: "image",
+                content: [
+                  "../assets/reddy.jpg"
+                ]
+              }
+            ]
+          },
+          {
+            metatype: "section",
+            content: []
+          }
         ]
       }
     ]
